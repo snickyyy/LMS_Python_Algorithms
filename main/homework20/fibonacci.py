fa27e786127bc1d982f2b7f16d5f43a8c072ba37
snickyyy <leon12345671234@gmail.com>
2024-10-16 23:03:29 +0200

    fix fibonacci tests

--- main/homework20/fibonacci.py
+++ main/homework20/fibonacci.py
@@ -16,15 +16,14 @@ class Fibonacci:
             # Compute and cache the requested Fibonacci number
             fib_number = self(n - 1) + self(n - 2)
             self.cache.append(fib_number)
-            print(self.cache, n)
 
         return self.cache[n]
 
 
 class TestFibonacci(unittest.TestCase):
     def test_zero_number(self):
-        with self.assertRaises(ValueError):
-            Fibonacci()(0)
+        fibonacci = Fibonacci()
+        self.assertEqual(fibonacci(0), 0)
 
     def test_negative_limit(self):
         with self.assertRaises(ValueError):
@@ -38,23 +37,23 @@ class TestFibonacci(unittest.TestCase):
 
     def test_large_number(self):
         fibonacci = Fibonacci()
-        self.assertEqual(fibonacci(100), 354_224_848_179_261_915_075)
+        self.assertEqual(fibonacci(500), 139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125)
 
     def test_float_raise(self):
         with self.assertRaises(ValueError):
             Fibonacci()(1.1)
 
     def test_type_raise(self):
-        with self.assertRaises(TypeError): Fibonacci()('1')
-        with self.assertRaises(TypeError): Fibonacci()(None)
-        with self.assertRaises(TypeError): Fibonacci()([1, 2, 3])
-        with self.assertRaises(TypeError): Fibonacci()((1, 2, 3))
-        with self.assertRaises(TypeError): Fibonacci()({1: 2})
-        with self.assertRaises(TypeError): Fibonacci()({1, 2, 3, 4})
+        with self.assertRaises(ValueError): Fibonacci()('1')
+        with self.assertRaises(ValueError): Fibonacci()(None)
+        with self.assertRaises(ValueError): Fibonacci()([1, 2, 3])
+        with self.assertRaises(ValueError): Fibonacci()((1, 2, 3))
+        with self.assertRaises(ValueError): Fibonacci()({1: 2})
+        with self.assertRaises(ValueError): Fibonacci()({1, 2, 3, 4})
 
 def main():
+    # print(Fibonacci()(0))
     unittest.main()
-
 
 if __name__ == "__main__":
     main()
